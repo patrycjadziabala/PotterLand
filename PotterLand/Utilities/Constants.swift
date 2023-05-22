0e4b3efb2cd8ab2178e75f9b694a4a2f077599c8
Patrycja Dziabala <patrycja.dziabala@gmail.com>
2023-05-22 13:35:08 +0200

    Add custom colors

---
+++ PotterLand/Utilities/Constants.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  PotterLand
+//
+//  Created by Patka on 22/05/2023.
+//
+
+import UIKit
+import Foundation
+
+struct Constants {
+    
+    // MARK: - Colors
+    
+    static let customBlack = "CustomBlack"
+    static let customBlue = "CustomBlue"
+    static let customBronze = "CustomBronze"
+    static let customGold = "CustomGold"
+    static let customGreen = "CustomGreen"
+    static let customRed = "CustomRed"
+    static let customSilver = "CustomSilver"
+    static let customYellow = "CustomYellow"
+    
+}
